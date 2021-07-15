8615455f703e51ce90423aebe5d60254f88857a0
jason21125 <n26090130@ncku.edu.tw>
2021-07-15 22:00:04 +0800

    add aichatbot branch

--- env_setup.sh
+++ env_setup.sh
@@ -4,8 +4,8 @@ GIT_EMAIL=<your git email>
 GITLAB_LOGIN=<your playlab gitlab login name>
 
 # project parameters, must be consistent with gitlab URLs
-RUN_FLASK=false                                 # start docker env with / without uWSGI and nginx proxy
-COURSE="aica-spring-2020"
-PROJECT="aica_lab4,aica_lab5"                   # projects without flask
-FLASK_PROJECT="lab6_line_server"                # flask projects
-CURRENT_FLASK_FOLDER="www/lab6_line_server"     # mount to /workspace/www in container
+RUN_FLASK=true                                 # start docker env with / without uWSGI and nginx proxy
+COURSE="aichatbox_fall_2021"
+PROJECT="test"                   # projects without flask
+FLASK_PROJECT="lab1,linebot_setup"                # flask projects
+CURRENT_FLASK_FOLDER="www/linebot_setup"     # mount to /workspace/www in container
