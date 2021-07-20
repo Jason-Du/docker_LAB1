533d0154a42673d9f6976562cc0e4fdfe007e9fe
jason21125 <n26090130@ncku.edu.tw>
2021-07-20 14:44:13 +0800

    update env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,16 +1,16 @@
 # personal settings
-GIT_NAME="Wei-Fen Lin"
-GIT_EMAIL=weifen.lin@gs.ncku.edu.tw
-GITLAB_LOGIN=weifen
+GIT_NAME="jason21125"
+GIT_EMAIL="n26090130@ncku.edu.tw"
+GITLAB_LOGIN="jason21125"
 
 # docker configuration
-COURSE="base"
+COURSE="aichatbot"
 PORT_MAPPING="3000:3000"                        # host:container
 NGINX_PORT="8080"                               # host
 
 # project parameters, must be consistent with gitlab URLs
-RUN_FLASK=false                                 # start docker env with / without uWSGI and nginx proxy
-COURSE_GITLAB="aica-spring-2020"
-PROJECT="aica_lab4"                   # projects without flask
-FLASK_PROJECT="lab6_line_server"                # flask projects
-CURRENT_FLASK_FOLDER="www/lab6_line_server"     # mount to /workspace/www in container
+RUN_FLASK=true                                 # start docker env with / without uWSGI and nginx proxy
+COURSE_GITLAB="aichatbot_fall_2021"
+PROJECT="test"                   # projects without flask
+FLASK_PROJECT="lab1,linebot_setup"                # flask projects
+CURRENT_FLASK_FOLDER="www/linebot_setup"     # mount to /workspace/www in container
